765e46b4409768a07f610e05f8c289a13e14bd68
Stephen Lalor <stephen.lalor.work@gmail.com>
2024-06-09 19:09:02 +0100

    Added log for a rag run and more utils.

--- treatment_checker/utils.py
+++ treatment_checker/utils.py
@@ -5,3 +5,13 @@ def read_json(filename):
     with open(filename, "r") as f:
         data = json.load(f)
     return data
+
+
+def read_text(filename):
+    with open(filename, "r") as f:
+        data = f.read()
+    return data
+
+
+def split_on_nl(x):
+    return x.split("\n")
